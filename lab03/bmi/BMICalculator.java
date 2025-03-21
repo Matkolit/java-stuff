7c05cb3ad4b77b4a66e4fefb32c0f02eeb45ea05
Mateusz <mateusz4k@outlook.com>
2025-03-21 21:27:56 +0100

    add risk level calculation and improve BMI output in BMICalculator

--- lab03/bmi/BMICalculator.java
+++ lab03/bmi/BMICalculator.java
@@ -19,13 +19,29 @@ public class BMICalculator {
     public boolean isOverweight() {
         return (bmi > 25);
     }
+    public String getRiskLevel() {
+        double[] scale = {18.49, 24.99, 29.99, 34.99, 39.99};
+
+        String[] riskLevels = {
+            "minimalne, ale zwiększony poziom wystąpenia innych problemów zdrowotnych",
+            "minimalne",
+            "średnie",
+            "wysokie",
+            "bardzo wysokie",
+            "ekstremalny poziom ryzyka"
+        };
+
+        return riskLevels[getColumnIndex(scale)];
+    }
 
     public String getName() {
         return name;
     }
 
     public String getBmiCategory() {
-        String[] category = {
+        double[] scale = {16, 16.99, 18.49, 24.99, 29.99, 34.99, 39.99};
+
+        String[] categories = {
             "wygłodzenie",
             "wychudzenie",
             "niedowaga",
@@ -35,22 +51,19 @@ public class BMICalculator {
             "otyłość II stopnia (duża)",
             "otyłość III stopnia (chorobliwa)"
         };
-
-        double[] scale = {16, 16.99, 18.49, 24.99, 29.99, 34.99, 39.99};
-        int currentIdx = 0;
-
-        while (currentIdx < scale.length && bmi > scale[currentIdx]) {
-            currentIdx++;
-        }
-
-        System.out.println(category[currentIdx]);
-
-        System.out.println(bmi);
-        return name;
+        return categories[getColumnIndex(scale)];
     }
 
     public double getBMI() {
         return bmi;
     }
 
-} 
+    private int getColumnIndex(double[] scale) {
+
+        int currentIdx = 0;
+        while (currentIdx < scale.length && bmi > scale[currentIdx]) {
+            currentIdx++;
+        }
+        return currentIdx;
+    }
+}
--- lab03/bmi/Test.java
+++ lab03/bmi/Test.java
@@ -7,42 +7,40 @@ public class Test {
         MyData.info();
 
         try {
-            Scanner sc = new Scanner(System.in);
+            try (Scanner sc = new Scanner(System.in)) {
+                System.out.print("\nPodaj imię: ");
+                String name = sc.nextLine();
 
-            System.out.print("\nPodaj imię: ");
-            String name = sc.nextLine();
+                System.out.print("Podaj wzrost(m): ");
+                float height = Float.parseFloat(sc.nextLine());
 
-            System.out.print("Podaj wzrost(m): ");
-            float height = Float.parseFloat(sc.nextLine());
+                System.out.print("Podaj masę(kg): ");
+                int weight = Integer.parseInt(sc.nextLine());
 
-            System.out.print("Podaj masę(kg): ");
-            int weight = Integer.parseInt(sc.nextLine());
+                BMICalculator calc1 = new BMICalculator(name, weight, height);
 
-            BMICalculator calc1 = new BMICalculator(name, weight, height);
+                calc1.calculateBMI();
 
-            calc1.calculateBMI();
-            
-            calc1.getBmiCategory();
+                System.out.print(calc1.getName() + " - twoje BMI: ");
+                System.out.format("%.2f\n", calc1.getBMI());
+                System.out.println("Kategoria wagowa: " + calc1.getBmiCategory());
+                System.out.println("Ryzyko zachorowań: " + calc1.getRiskLevel());
 
-            
-            System.out.print(calc1.getName() + " - twoje BMI: ");
-            System.out.format("%.2f\n", calc1.getBMI());
-            System.out.println("Nadwaga: " + calc1.isOverweight());
-            
-            System.out.print("\nPodaj imię: ");
-            name = sc.nextLine();
-            System.out.print("Podaj wzrost(m): ");
-            height = Float.parseFloat(sc.nextLine());
-            System.out.print("Podaj masę(kg): ");
-            weight = Integer.parseInt(sc.nextLine());
+                System.out.print("\nPodaj imię: ");
+                name = sc.nextLine();
+                System.out.print("Podaj wzrost(m): ");
+                height = Float.parseFloat(sc.nextLine());
+                System.out.print("Podaj masę(kg): ");
+                weight = Integer.parseInt(sc.nextLine());
 
-            BMICalculator calc2 = new BMICalculator(name, weight, height);
-            calc2.calculateBMI();
+                BMICalculator calc2 = new BMICalculator(name, weight, height);
+                calc2.calculateBMI();
 
-            System.out.print(calc2.getName() + " - twoje BMI: ");
-            System.out.format("%.2f\n", calc2.getBMI());
-            System.out.println("Nadwaga: " + calc2.isOverweight());
-            sc.close();
+                System.out.print(calc2.getName() + " - twoje BMI: ");
+                System.out.format("%.2f\n", calc2.getBMI());
+                System.out.println("Kategoria wagowa: " + calc2.getBmiCategory());
+                System.out.println("Ryzyko zachorowań: " + calc2.getRiskLevel());
+            }
 
         } catch (NumberFormatException e) {
             System.err.println("Error: " + e.getMessage());
